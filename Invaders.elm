33507ccd2da5bf610c7fdd2ae15f852df8d9328e
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2014-10-06 16:59:33 -0300

    Ship shoots missiles and hits enemies

--- Invaders.elm
+++ Invaders.elm
@@ -2,6 +2,7 @@ module Invaders where
 
 import Window
 import Keyboard
+import Maybe
 
 {-- Part 1: Model the user input ----------------------------------------------
 
@@ -13,10 +14,11 @@ Task: Redefine `UserInput` to include all of the information you need.
 
 ------------------------------------------------------------------------------}
 
-type UserInput = {direction: Int}
+type UserInput = {direction: Int, shooting: Bool}
 
 userInput : Signal UserInput
 userInput = UserInput <~ lift .x Keyboard.arrows
+                       ~ Keyboard.space
 
 type Input = { timeDelta:Float, userInput:UserInput }
 
@@ -40,16 +42,26 @@ be an empty list (no objects at the start):
 (gameWidth, gameHeight) = (800, 600)
 (halfWidth, halfHeight) = (gameWidth/2, gameHeight/2)
 shipSpeed = 1/6
+shipMissileSpeed = 1/4
 
+-- Components
 type Position obj = {obj | x:Float, y:Float}
-type VelocityX obj = {obj | vx:Float}
 type Size obj = {obj | w:Float, h:Float}
-type Ship = Position (VelocityX (Size {}))
+type VelocityX obj = {obj | vx:Float}
+type VelocityY obj = {obj | vy:Float}
+
+-- Entities
+type Ship = Position (Size (VelocityX {}))
+type ShipMissile = Position (Size (VelocityY {}))
 type Enemy = Position (Size {})
-type GameState = {ship: Ship, enemies: [Enemy]}
+
+type GameState = {ship: Ship, enemies: [Enemy], shipMissile: Maybe ShipMissile}
 
 defaultGame : GameState
-defaultGame = {ship={x=0, y=-halfHeight + 30, w=30, h=30, vx=0}, enemies=defaultEnemies}
+defaultGame = {ship=defaultShip, enemies=defaultEnemies, shipMissile=Nothing}
+
+defaultShip : Ship
+defaultShip = {x=0, y=-halfHeight + 30, w=30, h=30, vx=0}
 
 defaultEnemies : [Enemy]
 defaultEnemies =
@@ -67,11 +79,59 @@ Task: redefine `stepGame` to use the UserInput and GameState
 
 ------------------------------------------------------------------------------}
 
+flatMapMaybe : (a -> Maybe b) -> Maybe a -> Maybe b
+flatMapMaybe f m = case (Maybe.map f m) of
+  Just val -> val
+  Nothing -> Nothing
+
 stepGame : Input -> GameState -> GameState
-stepGame {timeDelta, userInput} ({ship} as game) =
+stepGame {timeDelta, userInput} ({ship, shipMissile, enemies} as game) =
+      -- First move the entities
   let ship' = moveShip userInput.direction timeDelta ship
+
+      shipMissile' = case shipMissile of
+        Just missile -> Just <| moveMissile timeDelta missile
+        Nothing -> if userInput.shooting then Just (shootShipMissile ship) else Nothing
+
+      enemies' = map (moveEnemy timeDelta) enemies
+
+      -- Then detect collissions
+      enemies'' = case shipMissile of
+        Just missile -> filter (not << collide (moveMissile timeDelta missile)) enemies'
+        Nothing      -> enemies'
+
+      shipMissile'' = flatMapMaybe destroyIfOutOfBounds >> flatMapMaybe (destroyIfCollidesWithAny enemies) <| shipMissile'
   in
-    { game | ship <- ship' }
+    { game | ship <- ship', shipMissile <- shipMissile'', enemies <- enemies'' }
+
+shootShipMissile : Ship -> ShipMissile
+shootShipMissile {x,y} = {x=x, y=y,vy=shipMissileSpeed,w=10,h=20}
+
+collide : Position (Size a) -> Position (Size b) -> Bool
+collide a b =
+  let top i = i.y + i.h / 2
+      bottom i = i.y - i.h / 2
+      left i = i.x - i.w / 2
+      right i = i.x + i.w / 2
+      collidesHorizontally = ((left a) < (left b) && (left b) < (right a)) || ((left b) < (left a) && (left a) < (right b))
+      collidesVertically = ((bottom a) < (bottom b) && (bottom b) < (top a)) || ((bottom b) < (bottom a) && (bottom a) < (top b))
+  in collidesHorizontally && collidesVertically
+
+destroyIfCollidesWithAny : [Enemy] -> ShipMissile -> Maybe ShipMissile
+destroyIfCollidesWithAny enemies missile =
+  if (any (collide missile) enemies) then Nothing else Just missile
+
+destroyIfOutOfBounds : ShipMissile -> Maybe ShipMissile
+destroyIfOutOfBounds missile = if missile.y > halfHeight then Nothing else Just missile
+
+moveEnemy : Time -> Enemy -> Enemy
+moveEnemy t enemy = enemy
+
+moveMissile : Time -> ShipMissile -> ShipMissile
+moveMissile t ({y, vy, h} as missile) =
+  let vy' = shipMissileSpeed
+      y'  = y + (vy') * t
+  in {missile | y <- y', vy <- vy'}
 
 moveShip : Int -> Time -> Ship -> Ship
 moveShip direction t ({x, vx, w} as ship) =
@@ -90,18 +150,24 @@ Task: redefine `display` to use the GameState you defined in part 2.
 displayInPosition : Form -> Position a -> Form
 displayInPosition form obj = move (obj.x, obj.y) form
 
-shipShape ship = (filled white (rect ship.w ship.w))
+shipShape ship = (filled white (rect ship.w ship.h))
 displayShip ship = displayInPosition (shipShape ship) ship
+
+shipMissileShape missile = (filled white (rect missile.w missile.h))
+displayShipMissile missile = displayInPosition (shipMissileShape missile) missile
 
 enemyShape enemy = (filled red (rect enemy.w enemy.h))
 displayEnemy enemy = displayInPosition (enemyShape enemy) enemy
 
+wrapInList : a -> [a]
+wrapInList a = [a]
+
 display : (Int,Int) -> GameState -> Element
-display (w,h) {ship, enemies} = container w h middle <|
+display (w,h) {ship, enemies, shipMissile} = container w h middle <|
                             collage gameWidth gameHeight ([
                               filled black (rect gameWidth gameHeight),
                               displayShip ship
-                            ] ++ map displayEnemy enemies)
+                            ] ++ map displayEnemy enemies ++ Maybe.maybe [] (displayShipMissile >> wrapInList) shipMissile)
 
 
 
